9fbee4b03dc4fbc630f3166e867c66594c27be39
Oohrahmaki <nettie.smith@rocketmail.com>
2019-05-26 16:21:38 +0100

    refactor

--- script.js
+++ script.js
@@ -2,34 +2,22 @@ const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const date = new Date();
 
-function formatHours(){
-    if (date.getHours().length === 1){
+
+function formatTime(time){
+    if (time.length === 1){
         return "0" + date.getHours();
     }
-    else if (date.getHours().length === 0){
+    else if (time.length === 0){
         return "00";
     }
     else {
-        return date.getHours();
+        return time;
     }
 }
 
-function formatMinutes(){
-    if (date.getMinutes().length === 1){
-        return "0" + date.getMinutes();
-    }
-    else if (date.getMinutes().length === 0){
-        return "00";
-    }
-    else {
-        return date.getMinutes();
-    }
-};
-
-
 function getTime(){
-    hours.innerHTML = formatHours();
-    minutes.innerHTML = formatMinutes();
+    hours.innerHTML = formatTime(date.getHours());
+    minutes.innerHTML = formatTime(date.getMinutes());
 };
 
 getTime();
